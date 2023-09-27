d5af1d82667ab22833341d32c8eddbc0c2030032
Jeremy Wiebe <jeremy@khanacademy.org>
2023-09-27 09:46:58 -0700

    Remove mobile ios steps that are now done in mobile repo's setup.sh
    Test Plan: Run `./mac-ios-setup.sh` (if you don't have Khan/mobile#3249 checked out, it will error, otherwise it will run the setup in the mobile repo)

--- mobile-functions.sh
+++ mobile-functions.sh
@@ -4,28 +4,10 @@ set -e -o pipefail
 
 # This file should be sourced from within a Bash-ish shell
 
-install_homebrew_libraries() {
-    update "Installing Homebrew dependencies..."
-    # The mobile project requires these Homebrew packages
-    brew install pkg-config cairo libpng jpeg giflib pango zopfli
-
-    if ! brew tap | grep "getsentry/tools";
-    then
-        brew tap getsentry/tools
-    fi
-
-    brew install getsentry/tools/sentry-cli
-}
-
 # Ensure the Mobile Github repo is cloned.
 clone_mobile_repo() {
     if [ ! -d "$REPOS_DIR/mobile" ]; then
         update "Cloning mobile repository..."
         kaclone_repo git@github.com:Khan/mobile "$REPOS_DIR/" --email="$gitmail"
     fi
-}
-
-install_react_native_dependencies() {
-    update "Installing react-native dependencies..."
-    (cd "$REPOS_DIR/mobile/app"; yarn)
 }
