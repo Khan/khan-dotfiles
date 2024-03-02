4042290983404b359dc8b66599fccc1a3f67e642
Craig Silverstein <csilvers@khanacademy.org>
2024-03-02 10:14:20 -0800

    python-mode is not a thing

--- linux-setup.sh
+++ linux-setup.sh
@@ -247,7 +247,7 @@ EOF
     # Not needed for Khan, but useful things to have.
     sudo apt-get install -y ntp abiword diffstat expect gimp \
          mplayer netcat sysdig iftop tcpflow netpbm screen w3m \
-         vim emacs python-mode google-chrome-stable
+         vim emacs google-chrome-stable
 
     # If you don't have the other ack installed, ack is shorter than ack-grep
     # This might fail if you already have ack installed, so let it fail silently.
